9c741b911d613e47553528a6f3d684abcf5e4c1b
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-11 17:11:18 -0700

    is valid without a middle_initial and is not valid without a last_name and not valid without email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Not the proper email format!" }
   validates :password_hash, presence: true
 
   include BCrypt
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -15,8 +15,23 @@ describe User do
   end
 
   describe "validations" do
-    it 'is not valid without a username' do
+    it 'is not valid without a first_name' do
       user_example.first_name = ""
+      expect(user_example).to_not be_valid
+    end
+
+    it 'is valid without a middle_initial' do
+      user_example.middle_initial = ""
+      expect(user_example).to be_valid
+    end
+
+    it 'is not valid without a last_name' do
+      user_example.last_name = ""
+      expect(user_example).to_not be_valid
+    end
+
+    it 'is not valid without an email' do
+      user_example.email = ""
       expect(user_example).to_not be_valid
     end
   end
