90e3a8d203d49ece079238ed07d51f4586e4a74b
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-11 17:03:52 -0700

    is not valid without a first_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,28 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   validates :first_name, presence: true
-  validates :middle_initial, presence: true
   validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_hash, presence: true
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.login(email, password)
+    user = User.find_by_email(email)
+    if user.password == password
+      return user
+    else
+      nil
+    end
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 describe User do
   let(:user_example) do
     user_creation = User.new(
-        first_name: "John",
-        middle_initial: "P",
-        last_name: "Smith",
-        email: "jpsmith@example.com"
-      )
+      first_name: "John",
+      middle_initial: "P",
+      last_name: "Smith",
+      email: "jpsmith@example.com"
+    )
 
     user_creation.password = "passw0rd"
-    user_creation.save
+    user_creation.save!
     return user_creation
   end
 
   describe "validations" do
-    it 'is not valid without a username'
-    user_example.first_name = ""
-    expect(user_example).to_not be_valid
+    it 'is not valid without a username' do
+      user_example.first_name = ""
+      expect(user_example).to_not be_valid
+    end
   end
-
 end
