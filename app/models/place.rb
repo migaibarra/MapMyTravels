c9c14b5fd33bf42fde53ab748e01f786bb59aa1e
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-12 15:12:05 -0700

    creates a place model for the places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,8 @@
 class Place < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :name, presence: true
+  validates :country, presence: true
+
+  has_many :users, through: :users_places
+  has_many :hometown_people, class_name: User
+  has_many :citizens, class_name: User
 end
